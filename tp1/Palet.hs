9d8117fe878bd5a94f92ac76007ddd1160722deb
Santiago Pourteau <spourteau@udesa.edu.ar>
2025-03-11 11:09:05 -0300

    Inicio

---
+++ tp1/Palet.hs
@@ -0,0 +1,8 @@
+module Palet ( Palet, newP, destinationP, netP )
+  where
+
+data Palet = Pal String Int deriving (Eq, Show)
+
+newP :: String -> Int -> Palet   -- construye un Palet dada una ciudad de destino y un peso en toneladas
+destinationP :: Palet -> String  -- responde la ciudad destino del palet
+netP :: Palet -> Int             -- responde el peso en toneladas del palet
